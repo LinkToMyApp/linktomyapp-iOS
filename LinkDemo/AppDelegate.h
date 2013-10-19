9fdafa7eb5f67af1d7107a5c2a3fd4909196518c
Thibaut LE LEVIER <thibaut@lelevier.fr>
2013-10-19 14:35:02 +0200

    Initial Commit

---
+++ LinkDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LinkDemo
+//
+//  Created by Thibaut LE LEVIER on 19/10/2013.
+//  Copyright (c) 2013 Thibaut LE LEVIER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
