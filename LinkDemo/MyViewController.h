ff2d4332c13301a1c427fb130e764c953e5c0084
Thibaut LE LEVIER <thibaut@lelevier.fr>
2013-10-19 14:39:22 +0200

    add test controllers

---
+++ LinkDemo/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  linktomyapp-iOS
+//
+//  Created by Thibaut LE LEVIER on 19/10/2013.
+//  Copyright (c) 2013 Thibaut LE LEVIER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
---
+++ LinkDemo/MyViewController.m
@@ -0,0 +1,38 @@
+//
+//  MyViewController.m
+//  linktomyapp-iOS
+//
+//  Created by Thibaut LE LEVIER on 19/10/2013.
+//  Copyright (c) 2013 Thibaut LE LEVIER. All rights reserved.
+//
+
+#import "MyViewController.h"
+
+@interface MyViewController ()
+
+@end
+
+@implementation MyViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
